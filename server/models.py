365f16629e64e661af69baeda898f1a358cba3d4
Ben Cavins <ben.cavins@flatironschool.com>
2023-03-21 14:05:05 -0400

    add models

--- server/models.py
+++ server/models.py
@@ -1,10 +1,51 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
 
-db = SQLAlchemy()
 
-class Episode(db.Model):
-    __tablename__ = 'Episode'
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class Episode(db.Model, SerializerMixin):
+    __tablename__ = 'episodes'
+    serialize_rules = ('-appearances.episode', '-guests.episodes')
 
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    number = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-# add any models you may need. 
+    appearances = db.relationship('Appearance', backref='episode')
+    guests = association_proxy('appearances', 'episode', creator=lambda guest: Appearance(guest=guest))
+
+
+class Appearance(db.Model, SerializerMixin):
+    __tablename__ = 'appearances'
+    serialize_rules = ('-guests.appearance', '-episodes.appearance')
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'))
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'))
+
+
+class Guest(db.Model, SerializerMixin):
+    __tablename__ = 'guests'
+    serialize_rules = ('-episodes.guest', '-appearances.guests')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    occupation = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    appearances = db.relationship('Appearance', backref='guest')
+    episodes = association_proxy('appearances', 'guest', creator=lambda ep: Appearance(episode=ep))
